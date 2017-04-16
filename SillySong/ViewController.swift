3f8eea8eab9210fcbdab3a4417e123097e3bb284
Satveer Khera <satveersingh@outlook.com>
2017-04-16 16:33:56 -0500

    Removed commented code

--- SillySong/ViewController.swift
+++ SillySong/ViewController.swift
@@ -26,7 +26,7 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+      
     }
 
     @IBAction func rest(_ sender: Any) {
